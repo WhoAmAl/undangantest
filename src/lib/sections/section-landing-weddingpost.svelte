6ca4855b2b7b5741f52be860e6d5259ada775bfb
M. Aulia <muhamadauliarahmannasution@gmail.com>
2025-08-06 14:02:46 +0700

    WP Landing adjustment

--- src/lib/sections/section-landing-weddingpost.svelte
+++ src/lib/sections/section-landing-weddingpost.svelte
@@ -13,17 +13,15 @@
   }
 </style>
 
-<!-- Warna latar di luar box hanya aktif di md ke atas -->
-<!-- <div class="bg-black min-h-screen flex items-center justify-center md:px-4 md:py-10 px-0 py-0"> -->
-  <div class="w-full md:max-w-2xl mx-auto bg-[#dcdcdc] md:border md:border-black md:rounded-none px-4">
+<div class="w-full md:max-w-2xl mx-auto bg-[#dcdcdc] md:border md:border-black md:rounded-none md:px-4" style="font-family: 'Times New Roman', Times, serif;">
 
     <!-- Header Koran -->
     <div class="px-4 pt-3 text-center font-serif">
-      <p class="uppercase text-[24px] tracking-widest">THE BIGGEST WEDDING OF THE YEAR</p>
+      <p class="uppercase text-[14px] md:text-[24px] tracking-widest">THE BIGGEST WEDDING OF THE YEAR</p>
       <hr class="border-black border-[1.5px]" />
-      <p class="text-[24px]">12 Juni 2025</p>
+      <p class="text-[14px] md:text-[24px]">12 Juni 2025</p>
       <hr class="border-black border-[1.5px]" />
-      <h1 class="md:text-[46px]" style="font-family: 'Fleischman', serif;">THE WEDDING TIMES</h1>
+      <h1 class="text-[25px] md:text-[46px]" style="font-family: 'Fleischman', serif;">THE WEDDING TIMES</h1>
       <hr class="border-black border-t-2" />
       <hr class="border-black border-t-[1.5px] mt-0.5" />
     </div>
@@ -34,14 +32,14 @@
 
       <!-- Overlay -->
       <div class="absolute inset-0 flex flex-col justify-end text-white font-serif px-6 pb-6 md:rounded-md rounded-md">
-        <h2 class="text-center text-[36px] tracking-wide mb-2">HINATA &amp; NARUTO</h2>
+        <h2 class="text-center text-[24px] md:text-[36px] tracking-wide mb-2">HINATA &amp; NARUTO</h2>
 
         <!-- Garis hiasan -->
         <div class="border-t-[3px] border-white mb-[2px]"></div>
         <div class="border-t-[2px] border-white mb-2"></div>
 
         <!-- Undangan -->
-        <div class="flex justify-between text-[18px] px-35">
+        <div class="flex justify-between text-[12px] md:text-[18px] px-10 md:px-35">
           <span>Dear</span>
           <span>Alan and Partner</span>
         </div>
@@ -53,12 +51,11 @@
         <div class="flex justify-center">
           <button
             on:click={handleRedirect}
-            class="mb-10 bg-white border text-black border-white rounded-2xl px-6 py-2 text-sm shadow-sm hover:bg-gray-200 transition-all duration-200"
+            class="mb-10 bg-white border text-black border-white rounded-lg md:rounded-2xl px-3 md:px-6 py-1 md:py-2 text-sm shadow-sm hover:bg-gray-200 transition-all duration-200"
           >
             Open Invitation
           </button>
         </div>
       </div>
     </div>
-  </div>
-<!-- </div> -->
+</div>
